9d20e5289f64ee534da961a33ce85d21067eb6b9
ZeToF! <zetof@zetof.net>
2025-01-30 12:16:23 +0100

    Add LCD backlighting capabilities

--- armiclo.ino
+++ armiclo.ino
@@ -18,8 +18,14 @@
 // Definition of tempo LED pin
 #define TEMPO_LED_PIN 2
 
+// Definition of LCD backlighting pin
+#define BACKLIGHTING_PIN 10
+
 // Definition of LED blink time (en usecs)
 #define LED_BLINK_TIME 50000
+
+// Definition of LCD backlight ON time (en usecs)
+#define LCD_BACKLIGHT_TIME 10000000
 
 // Definition of user input delay (en usecs)
 #define USER_INPUT_DELAY 100000
@@ -36,12 +42,16 @@ int midiTicks;
 long lastMidiCheck;
 long lastLEDCheck;
 long lastUICheck;
+long lastBacklightCheck;
 
 // MIDI delay between each 24 ticks / beat
 long midiDelay;
 
 // LED blink config
 bool ledOnState = false;
+
+// LCD ON config
+bool lcdOnState = true;
 
 // Definition of LCD display
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -68,16 +78,21 @@ void setup() {
   // Set tempo led digital pin to output mode
   pinMode(TEMPO_LED_PIN, OUTPUT);
 
+  // Set LCD backlighting digital pin to output mode
+  pinMode(BACKLIGHTING_PIN, OUTPUT);
+
   // Init LCD in 2x16 mode
   lcd.begin(16, 2);
 
-  // Display splash title for 2s
-  lcd.print(F("  ARDUINO MIDI  "));
-  lcd.setCursor(2, 2);
-  lcd.print(F("SIMPLE CLOCK"));
+  // Display splash title for 3s
+  lcd.clear();
+  backlightOn(0);
+  lcd.print(F(" ARDUINO SIMPLE "));
+  lcd.setCursor(3, 2);
+  lcd.print(F("MIDI CLOCK"));
   delay(3000);
   lcd.clear();
-
+  
   // Reset all triggers before we start
   resetTriggers();
 
@@ -95,6 +110,7 @@ void resetTriggers(void) {
   lastMidiCheck = 0;
   lastLEDCheck = 0;
   lastUICheck = 0;
+  lastBacklightCheck = micros();
 }
 
 void dispatcher() {
@@ -103,8 +119,8 @@ void dispatcher() {
   // Time to read user input
   if(now - lastUICheck > USER_INPUT_DELAY) {
     lastUICheck = now;
-    rotaryVal = rotaryListener();
-    buttonListener();
+    rotaryVal = rotaryListener(now);
+    buttonListener(now);
   }
 
   // Time to send a MIDI clock signal
@@ -120,11 +136,15 @@ void dispatcher() {
 
   // Time to turn OFF LED
   if(ledOnState && now - lastLEDCheck > LED_BLINK_TIME) ledOff();
+
+  // Time to turn OFF LCD
+  if(lcdOnState && now - lastBacklightCheck > LCD_BACKLIGHT_TIME) backlightOff();
 }
 
-int rotaryListener(void) {
+int rotaryListener(long now) {
   int newRotaryVal = analogRead(rotaryPin);
   if(newRotaryVal != rotaryVal) {
+    backlightOn(now);
     int bpm = map(newRotaryVal, 0, 1020, 40, 300);
     lcd.setCursor(0, 1);
     lcd.print("BPM: " + String(bpm) + "  ");
@@ -144,11 +164,23 @@ void ledOff(void) {
   digitalWrite(TEMPO_LED_PIN, LOW);
 }
 
-void buttonListener() {
+void backlightOn(long now) {
+  lastBacklightCheck = now;
+  lcdOnState = true;
+  digitalWrite(BACKLIGHTING_PIN, HIGH);
+}
+
+void backlightOff(void) {
+  lcdOnState = true;
+  digitalWrite(BACKLIGHTING_PIN, LOW);
+}
+
+void buttonListener(long now) {
   int newButtonVal = analogRead(buttonPin);
   if (newButtonVal < BUTTON_RIGHT) {}
   else if (newButtonVal < BUTTON_UP) {
     if(buttonVal != BUTTON_UP) {
+      backlightOn(now);
       buttonVal = BUTTON_UP;
       if(playState == IS_PLAYING) {
         playState = IS_PAUSED;
@@ -166,6 +198,7 @@ void buttonListener() {
   }
   else if (newButtonVal < BUTTON_DOWN) {
     if(buttonVal != BUTTON_DOWN) {
+      backlightOn(now);
       buttonVal = BUTTON_DOWN;
       midiTicks = 0;
       playState = IS_PLAYING;
